a1e5ef0618b6fae60589db4d2f09ce7e1a58ce8a
SaumyaPython <saumya.parhi485@gmail.com>
2025-05-10 19:17:08 +0530

    new commit

--- pythonclass1.py
+++ pythonclass1.py
@@ -11,3 +11,20 @@ def check_palindrome(s):
 ip = "abcba"
 check_palindrome(ip)
 
+
+def find_duplicate_and_count(s):
+    op = {}
+    for i in s:
+        if i != " ":
+            if i not in op:
+                op[i] = 1
+            else:
+                op[i] += 1
+    print(op)
+    op_new = [(i,j) for i,j in op.items() if j>1]
+    return op_new
+
+
+s1 = "Python Automation is very easy"
+obj = find_duplicate_and_count(s1)
+print(obj)
