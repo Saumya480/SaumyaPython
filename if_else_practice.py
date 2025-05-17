652dd0b68006f7ab9e1e3ae9ddd28bd89856e5c7
SaumyaPython <saumya.parhi485@gmail.com>
2025-05-17 17:53:37 +0530

    If and for practice

---
+++ if_else_practice.py
@@ -0,0 +1,68 @@
+# Print "Hello World" if a is greater than b
+# a = 50
+# b = 10
+# if a>b:
+#     print("Hello world")
+
+# Print "Hello World" if a is not equal to b
+# a = 50
+# b = 10
+# if a !=b:
+#     print("Hello world")
+# Print "Yes" if a is equal to b, otherwise print "No".
+# a = 50
+# b = 10
+# if a ==b:
+#     print("Yes")
+# else:
+#     print("No")
+    # Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".
+# a = 50
+# b = 10
+# if a == b:
+#     print("1")
+# elif a >b:
+#     print("2")
+# else:
+#     print("3")
+    # Print "Hello" if a is equal to b, and c is equal to d
+# a = 50
+# b = 10
+# c = 50
+# d = 50
+# if a==b and c==d:
+#     print("Hello")
+# else:
+#     print("All the conditions should be True")
+    # Print "Hello" if a is equal to b, or if c is equal to d
+# a = 50
+# b = 10
+# c = 50
+# d = 50
+# if a==b or c==d:
+#     print("Hello")
+# else:
+#     print("Anyone of  the conditions should be True")
+#     This    example    misses    indentations    to    be    correct.
+    # Insert the missing indentation to make the code correct
+    # if 5 > 2:
+    #  print("Five is greater than two!")
+
+# if 5>2:
+#         print("Five is greater than Two")
+
+# Use the correct short hand syntax to put the following statement on one line
+# if 5 > 2:
+# print("Five is greater than two!")
+# if 5 > 2:print("Five is greater than two!")
+
+# Use the correct short hand syntax to write the following conditional expression in one line
+# if 5 > 2:
+# print("Yes")
+# else
+# print("No")
+# print("Yes") if 5 > 2 else print("No")
+
+
+
+
